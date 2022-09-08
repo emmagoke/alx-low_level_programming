fca215f144e4d73199a28e069225c6ae430dd71f
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-08 10:49:25 -0700

    Added new files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lo byte(s)\n", sizeof(char));
+	printf("Size of an int: %lo byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lo byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lo byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lo byte(s)\n", sizeof(float));
+	return (0);
+}
