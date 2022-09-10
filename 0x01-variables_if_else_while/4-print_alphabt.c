a5fa820e563ea807594fd280db4b233815ae35dd
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-09 19:39:13 -0700

    Added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int letter = 97, newline = 10;
+
+	while (letter < 123)
+	{
+		if (letter == 101)
+		{
+		}
+		else if (letter == 113)
+		{
+		}
+		else
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar(newline);
+	return (0);
+}
