0e200fe98af3b2d782ef672b1e18cdd7b75d36a0
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-13 16:05:19 -0700

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - This function print _putchar to stdout
+ * each value in _putchar represent an ascii value
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
