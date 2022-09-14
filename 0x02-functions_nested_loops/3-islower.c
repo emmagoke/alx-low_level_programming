a5ff48c3d93ae55bf3d0d3eac100c60a2f47122f
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-13 17:46:20 -0700

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is in lowercase
+ * @c: The character to be checked
+ *
+ * Return: 1(lowercase) or 0(not lowercase)
+ */
+int _islower(int c)
+{	
+	if (c >= 97 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
