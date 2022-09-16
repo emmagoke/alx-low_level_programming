8acdac64b01ee871f748649bd02fc97de20cfd47
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-15 19:25:04 -0700

    Added new files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Print _ n times
+ * @n: The number of times
+ * Return: void
+ */
+void print_line(int n)
+{
+	int line = 95, newline = 10, i;
+	if (n <= 0)
+	{
+		_putchar(newline);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(line);
+		}
+		_putchar(newline);
+	}
+}
