34061b8a75d973e62a6f90e254414b0bcf96e23c
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-15 20:06:54 -0700

    Added new files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Print hash square
+ * @c: The number of times
+ * Return: void
+ */
+void print_square(int n)
+{
+	int newline = 10, hash = 35;
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar(newline);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar(hash);
+			}
+			_putchar(newline);
+		}
+	}
+}
