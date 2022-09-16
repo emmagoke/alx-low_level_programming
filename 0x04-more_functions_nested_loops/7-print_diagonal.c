2e58468755c1f7299fabfe231d84646b4e77255e
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-15 19:46:05 -0700

    Added new files

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal
+ * @n: The number of times to print
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int space = 32, newline = 10, dia = 92;
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar(newline);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i+1; j++)
+			{
+				_putchar(space);
+			}
+			_putchar(dia);
+			_putchar(newline);
+		}
+	}
+}
