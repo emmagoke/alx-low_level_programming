f746c7059999f5b1256dd8f5ee03f5ec8cdf045f
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-19 13:43:35 -0700

    Added new files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - find the length of string
+ * @c: The string
+ * Return: The length of string c
+ */
+int _strlen(char *c)
+{
+	int len;
+
+	len = sizeof(c);
+	return (len);
+}
