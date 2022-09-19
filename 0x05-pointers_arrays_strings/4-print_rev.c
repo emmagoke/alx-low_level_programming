ebf731708e3a1a018c5b2326459438bc2489a628
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-19 15:48:24 -0700

    Added new files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a reverse string
+ * @s: The string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, newline = 10;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar(newline);
+}
