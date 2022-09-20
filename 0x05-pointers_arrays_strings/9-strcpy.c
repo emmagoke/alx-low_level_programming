620281d13f27e0a53f3a814971b4d2d2b2e41279
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-19 22:05:15 -0700

    Added new files

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy a string from one pointer to another
+ * @dest: The destination pointer
+ * @src: The string being copy
+ * Return: Returns a string
+ */
+char _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src != '\0', i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i + 1] = '\0';
+	return dest;
+}
