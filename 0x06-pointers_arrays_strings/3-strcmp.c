73011e0d324ba362cc08771313d61a95637f9c10
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-22 16:44:23 -0700

    Added question 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,36 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, s1_len, s2_len;
 	int s1_a = 0, s2_a = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	if (s1_len == 0)
 	{
-		if (s1[i] == s2[2])
+		s2_a = s2[0];
+		return (s1_a - s2_a);
+	}
+	else if (s2_len == 0)
+	{
+		s1_a = s1[0];
+		return (s1_a - s2_a);
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 		{
-			continue;
-		}
-		else
-		{
-			s1_a = s1[i];
-			s2_a = s2[i];
-			/* substract the ascii value of the non-matching string */
-			return (s1_a - s2_a);
+			if (s1[i] == s2[2])
+			{
+				continue;
+			}
+			else
+			{
+				s1_a = s1[i];
+				s2_a = s2[i];
+				/* substract the ascii value of the non-matching string */
+				return (s1_a - s2_a);
+			}
 		}
 	}
 	return (0); /* The two strings are equal */
