3d762233402bf73507acdd2d1217910000642469
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-22 16:24:13 -0700

    Added question 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - Compares two string together
+ * @s1: The first string
+ * @s2: The second string
+ * Return: An integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, s1_len, s2_len;
+	int s1_a = 0, s2_a = 0;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	for (i = 0; (i < s1_len) && (i < s2_len); i++)
+	{
+		if (s1[i] == s2[2])
+		{
+			continue;
+		}
+		else
+		{
+			s1_a = s1[i];
+			s2_a = s2[i];
+			/* substract the ascii value of the non-matching string */
+			return (s1_a - s2_a);
+		}
+	}
+	return (0); /* The two strings are equal */
+}
