97fdf1feb0fd079883e40d5b7b15a871da80cc9b
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-22 20:04:37 -0700

    Added question 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,8 @@
  */
 char *cap_string(char *str)
 {
-	int i, newline = 10;
-	int tab = 9, l_b = 123, r_b = 125;
+	int i, newline = 10, space = 32;
+	int l_b = 123, r_b = 125;
 
 	if (str[0] >= 97 && str[0] <= 122)
 	{
@@ -18,7 +18,10 @@ char *cap_string(char *str)
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 32 && str[i] <= 46) || (str[i] == newline || str[i] == tab || str[i] == l_b || str[i] ==  r_b))
+		if ((str[i] == space || str[i] == '\t' || str[i] == '.' || str[i] == '"'
+					|| str[i] == '?' || str[i] == '(' || str[i] == ')'
+					|| str[i] == newline || str[i] == '!'
+					|| str[i] == l_b || str[i] ==  r_b))
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
