d82dcb328a959438f20c7e6d46536927a911a576
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-26 20:08:19 -0700

    Added question 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,15 +11,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, len = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				len += 1;
+				break;
 			}
 		}
+		if (accept[j] == '\0')
+			return (len);
 	}
 	return (len);
 }
