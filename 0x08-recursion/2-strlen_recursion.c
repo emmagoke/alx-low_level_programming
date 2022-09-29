5077f83be6b94e78c89c51abf51cae676d896938
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-28 19:06:29 -0700

    Added question 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,16 +8,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s == '\0')
-		return (0);
-	else
-	{
-		if (*s != '\0')
-		{
-			i = 1 + _strlen_recursion(s + 1);
-		}
-	}
-	return (i);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
