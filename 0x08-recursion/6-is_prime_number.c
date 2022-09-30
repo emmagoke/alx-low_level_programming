c3db70b0d880958eecd61f83a4084e650abc0d22
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-29 17:08:22 -0700

    Added question 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int _divide(int x, int y);
+/**
+ * is_prime_number - checks if anumber n is prime
+ * @n: The number to be checked
+ * Return: 1 (if n is prime) or 0 (if not)
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (_divide(n, 2));
+}
+
+/**
+ * _divide - Check if a number x is divisible 
+ * by y upto y + 1 < n
+ * @x: An Integer
+ * @y: An integer
+ * Return: 1(if divisble) or 0 (if not)
+ */
+int _divide(int x, int y)
+{
+	if (y < x)
+	{
+		if (x % y == 0)
+			return (0);
+		else
+			return (_divide(x, (y + 1)));
+	}
+	return (1);
+}
