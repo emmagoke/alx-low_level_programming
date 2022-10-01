5df68d645344f2cb809fa77fca30fb67643f20fc
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-09-30 17:14:48 -0700

    Added question 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the multiplication of command line
+ * arguments except the first.
+ * @argc: The number of command line arguments
+ * @argv: arrays of command line arguments
+ */
+int main(int argc, char *argv[])
+{
+	int result = 1, i, x;
+
+	if (argc < 2 && **argv)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			x = strtol(argv[i], NULL, 10);
+			result *= x;
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+}
