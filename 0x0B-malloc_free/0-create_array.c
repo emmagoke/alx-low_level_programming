8febc66992fff67f18ce574fb560a216a2b5ac60
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-03 14:27:13 -0700

    made corrections to 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return ('\0');
-	buffer = (char*)malloc(size * sizeof(c));
+	buffer = malloc(size * sizeof(c));
 
 	for (i = 0; i < size; i++)
 		buffer[i] = c;
