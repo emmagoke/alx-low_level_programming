6abbc58d90d38f7b1a39c41b1fbeaa6db7c8bae2
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-06 19:10:48 -0700

    Made Corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j;
 
+	if (*s1 == NULL)
+		*s1 = "";
+	if (*s2 == NULL)
+		*s2 = "";
+
 	str = malloc((strlen(s1) + n + 1));
 
 	if (str == NULL)
@@ -28,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for(j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 			str[i + j] = s2[j];
 	}
 	str[i + j] = '\0';
