9895c716a60b5aeb44aef0a673d9712de48c20b1
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-10 17:56:04 -0700

    Made corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - This is dog type with parameter name, age an owner.
+ * @name: This is a charater pointer
+ * @age: This is a float
+ * @owner: This is a character pointer.
+ *
+ * Description: This dog type creates a dog with its name,
+ * its age and the name of it's owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
