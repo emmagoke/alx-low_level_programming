3cfcb68d9b7774a937953bb867acbd4ca6128e7d
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-10 19:31:21 -0700

    Solved question 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints the value of struct dog type
+ * followed by a new line.
+ * @d: The struct dog object.
+ * Return: Nothing.
+ */
+void print_dog(struct dog *d)
+{
+	struct dog new = *d;
+
+	if (d == NULL)
+	{
+		;
+	}
+	else
+	{
+		if (new.name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		if (new.age == 0.0)
+			printf("Age: (nil)\n");
+		if (new.owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", new.name);
+			printf("Age: %f\n", new.age);
+			printf("Owner: %s\n", new.owner);
+		}
+	}
+}
