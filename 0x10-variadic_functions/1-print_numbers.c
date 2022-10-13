192c53ec1540ea2177c5a2de4a1f5aeda0b7d738
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-13 14:08:12 -0700

    Made corrections

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1))
 		{
 			printf("%d", num);
-			if (*separator)
+			if (separator)
 				printf("%s", separator);
 		}
 		else
