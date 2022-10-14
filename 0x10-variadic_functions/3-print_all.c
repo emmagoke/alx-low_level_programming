a56e9cfd888021180ccb3db1af388186ae68e56f
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-13 20:36:51 -0700

    Added question 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_int - Prints an integer value
+ * @arg: This is va_list type
+ * Return: Nothing
+ */
+void print_int(va_list arg)
+{
+	int num = va_arg(arg, int);
+
+	printf("%d", num);
+}
+
+/**
+ * print_char - Prints a character
+ * @arg: This is va_list type
+ * Return: Nothing
+ */
+void print_char(va_list arg)
+{
+	int c = va_arg(arg, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_float - Prints a float
+ * @arg: This is va_list type
+ * Return: Nothing
+ */
+void print_float(va_list arg)
+{
+	float num = va_arg(arg, double);
+
+	printf("%f", num);
+}
+
+/**
+ * print_string - Prints a string
+ * @arg: This is va_list type
+ * Return: Nothing
+ */
+void print_string(va_list arg)
+{
+	char *s = va_arg(arg, char *);
+
+	if (s)
+	{
+		printf("%s", s);
+		return;
+	}
+	printf("(nil)");
+}
+
+/**
+ * print_all - This function prints anything
+ * @format: is a list of types of arguments passed to the function
+ * (c > char, i > integer, f > float, s > char *)
+ * (if the string is NULL, print (nil) instead
+ * any other char should be ignored
+ * Return: Nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	format_t type[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+	int i, j;
+	char *sep = "";
+
+	va_start(args, format);
+	i = 0;
+	while (*(format + i))
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (*(format + i) == *type[j].type)
+			{
+				printf("%s", sep);
+				type[j].f(args);
+				sep = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct format_t - Struct format_t
+ *
+ * @type: The type of elemented to be formatted.
+ * @f: A function pointer to a function that
+ * take av_list from stdarg.h
+ */
+
+typedef struct format_t
+{
+	char *type;
+	void (*f)(va_list arg);
+} format_t;
+
+void print_char(va_list arg);
+void print_float(va_list arg);
+void print_char(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
 #endif
