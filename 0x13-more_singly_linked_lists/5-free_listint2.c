4fa29f8e9ba78923b6732b9ac82aad3f2e3f1219
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-25 16:41:25 -0700

    Made corrections

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t **temp2 = head;
 
-	while (*head != NULL)
+	while (*temp2 != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = *temp2;
+		*temp2 = (*temp2)->next;
 		free(temp);
 	}
-	head = NULL;
+	temp2 = NULL;
 }
