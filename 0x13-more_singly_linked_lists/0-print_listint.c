fc6f4dbd5f821a04305b53369da9e472f1737cc5
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-25 13:09:52 -0700

    Added question 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints each integer in each node
+ * @h: The end of the linked list.
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		n += 1;
+	}
+
+	return (n);
+}
