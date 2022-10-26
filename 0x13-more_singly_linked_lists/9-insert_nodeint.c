0ec5df3ab60d54cc973cbb07126eae2991a9359b
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-25 20:49:42 -0700

    Made corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,10 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (new == NULL || idx > len(*head))
-	{
-		free(new);
 		return (NULL);
-	}
 
 	new->n = n;
 	new->next = NULL;
