8fe73b13779b9bd7daad2093ed3f451dd0b20d3c
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-10-27 20:05:53 -0700

    Made corrections

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,9 +12,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int out;
 
-	if (index > 63)
+	if (n == NULL && index > 63)
 		return (-1);
-	out = (((*n >> index) & 1) | 1);
+	out = (((*n >> index)  & 1) | 1);
 	*n = (*n ^ (out << index));
 	return (*n);
 }
