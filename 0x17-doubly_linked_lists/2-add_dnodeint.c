f63f131020eb08fa63c375884451971e8123b7eb
Emmanuel Olagoke <emma.goke05@gmail.com>
2022-12-08 11:50:45 -0800

    Added the solution to question 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - This function adds a new node at the
+ * beginning of a dlistint_t list.
+ * @head: The pointer to head of the list
+ * @n: The value to be adde to the node.
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = getnode(n);
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (*head);
+	}
+	(*head)->prev = newnode;
+	newnode->next = *head;
+	*head = newnode;
+	return (*head);
+}
+
+/**
+ * getnode - This create a new node from the heap
+ * @n: The value to be added to the new node.
+ * Return: The pointer to the newly created node.
+ */
+dlistint_t *getnode(const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		printf("Malloc error");
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
