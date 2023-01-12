5ab4e1b65660de83f18a23df8a2caa856ae35c2f
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-01-11 17:07:01 -0800

    Added question 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned int i;
+	/*unsigned int i;*/
 
 	if (table == NULL)
 		return (NULL);
